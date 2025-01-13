cfcfea011bd77d26adadafe8d58e0caedc17fbd0
alaindlctvop <alain_dlc@outlook.com>
2025-01-13 18:05:08 +0100

    statement logic

--- First/statement.py
+++ First/statement.py
@@ -3,6 +3,10 @@
 # []  detta är en lista
 # Lista = [1, 2, 3, 4, 5 ]
 # Index = [0', 1, 2, 3, 4 ]
-hej = [1, 2, 3, 4, 5 ]
-hej.append(6)
-print(hej[4])
+#hej = [1, 2, 3, 4, 5 ]
+#hej.append(6)
+#print(hej[4])
+
+hej = 3
+if hej !=3:
+    print("ja det är det")
