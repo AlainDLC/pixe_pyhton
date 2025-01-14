7545531e479cc8b92475a4f06f54fb74595556ba
alaindlctvop <alain_dlc@outlook.com>
2025-01-14 20:57:59 +0100

    if else statement

--- First/statement.py
+++ First/statement.py
@@ -7,6 +7,20 @@
 #hej.append(6)
 #print(hej[4])
 
-hej = 3
-if hej !=3:
-    print("ja det är det")
+#hej = 3
+#if hej !=3:
+    #print("ja det är det")
+
+
+
+
+#hej=input("ange ett heltal:")
+#hej = int(hej)  # Konverterar strängen till ett heltal
+#if hej >= 1:
+    #print("hej")
+
+mitt_namn =str(input("Skriv ditt namn "))
+if mitt_namn == "Leon":
+    print("Ditt namn är Leon")
+else : 
+    print("Ditt namn är inte Leon")
